2b6cd17f5fb4f7f274630f7f379dcad7ae4e2c99
Rémi Deschamps <Remideschamps2662@gmail.com>
2024-09-02 16:01:20 +0200

    version finale

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
-'use strict'
+'use strict';
 
-let burgerToolBar = document.querySelector('.menu_icons');
-let navigationLinks = document.querySelector('.menu ul');
-let menu = document.querySelector('.menu');
-let header = document.querySelector('header');
-let logo = document.querySelector
+const burgerToolBar = document.querySelector('.menu_icons');
+const navigationLinks = document.querySelector('.menu ul');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('header');
+const buttonClickCollect = document.querySelector('div.header-end button');
 let toggler = 0;
 
 document.addEventListener('DOMContentLoaded',()=>{
     burgerToolBar.addEventListener('click',()=>{
-        if((toggler == 0)){
+        if((toggler === 0)){
+            if(burgerToolBar.classList.contains('noRotate')){
+                burgerToolBar.classList.replace('noRotate','rotate');
+            }
+            else{
+                burgerToolBar.classList.add('rotate');
+            }
+            buttonClickCollect.style.display="flex";
             header.style.flexDirection="column";
             menu.style.flexDirection ="column";
             navigationLinks.style.display = "flex";
@@ -17,6 +24,7 @@ document.addEventListener('DOMContentLoaded',()=>{
             toggler = 1;
         }
         else{
+            burgerToolBar.classList.replace('rotate','noRotate');
             navigationLinks.style.display ="none";
             menu.style.flexDirection ="row";
             navigationLinks.style.flexDirection = "row";
@@ -24,4 +32,27 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
     })
 
-})
+    window.addEventListener('resize',()=>{
+        if(window.matchMedia("(min-width:769px)").matches){
+            navigationLinks.style.display ="flex";
+            header.style.flexDirection="row";
+            menu.style.flexDirection ="row";
+            navigationLinks.style.flexDirection ="row";
+            toggler = 0;
+        }
+        else{
+            if((toggler === 0)){
+                burgerToolBar.classList.replace('rotate','noRotate');
+                navigationLinks.style.display ="none";
+            }
+            else{
+                header.style.flexDirection="column";
+                menu.style.flexDirection ="column";
+                navigationLinks.style.display = "flex";
+                navigationLinks.style.flexDirection = "column";
+            }
+            
+        }
+    })
+  
+});
